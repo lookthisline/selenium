9c92418a42a9bfcfbd3bb00cd1b6b899862eb06e
lookthisline <liujiawen.personal@outlook.com>
2023-10-07 12:26:13 +0800

    update: 调整文件保存策略

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from driver.chrome import chrome_driver
-from spider.wn import get_element_by_xpath, url, next_page
 from db.session import sqlite_session
+from db.sqlite.picture import fine_record, save_data
+from driver.chrome import chrome_driver
 from loguru import logger
+from spider.wn import get_element_by_xpath, url, next_page
 from sqlalchemy.orm import Session
 from utils.document import save_media
-from db.sqlite.picture import fine_record, save_data
+import hashlib
 import os
+import re
+import time
 
 
 def save_page_data(session: Session, res: list = []):
@@ -22,7 +25,21 @@ def save_pic(res: list = []):
         # 获取文件后缀
         suffix = item['pic_uri'].split(".")[-1]
         file_name = item['pic_title'][:250] + "." + suffix  # 限制文件名长度，预留5个字符给后缀
-        save_media(item['pic_uri'], SAVE_DIR + file_name)
+        # 备选名称
+        file_name_2 = re.findall(
+            '\d+', item["page_link"])[0]  # 提取其中的数字部分
+        if not file_name_2:
+            # 使用hash时间字符串为文件名
+            file_name_2 = hashlib.md5(
+                str(time.time()).encode('utf-8')).hexdigest()
+        try:
+            save_media(item['pic_uri'], SAVE_DIR + file_name)  # 保存到正常名称
+        except OSError as e:
+            logger.error(e)
+            save_media(item["pic_uri"], SAVE_DIR +
+                       file_name_2 + suffix)  # 保存到备选名称
+        except Exception as e:
+            logger.exception(e)
 
 
 # 保存到当前目录的images文件夹下
--- utils/document.py
+++ utils/document.py
@@ -30,36 +30,33 @@ def _progress(block_num, block_size, total_size):
 
 def save_media(uri: str, save_path: str):
     """
-    http保存媒体文件
+    http保存媒体文件，异常抛至上层
+    :param uri: 网络地址
+    :param save_path: 保存路径
     """
-    try:
-        # 获取save_path的文件夹路径
-        dir_path = os.path.dirname(save_path)
-        # 如果文件夹不存在则创建
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
+    # 获取save_path的文件夹路径
+    dir_path = os.path.dirname(save_path)
+    # 如果文件夹不存在则创建
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
 
-        # 判断文件是否存在
-        if os.path.exists(save_path):
-            print("文件已存在")
-            return
+    # 判断文件是否存在
+    if os.path.exists(save_path):
+        print("文件已存在")
+        return
 
-        # urllib配置代理
-        proxy_handler = urllib.request.ProxyHandler({
-            "http": "http://127.0.0.1:7890/",
-            "https": "https://127.0.0.1:7890/"
-        })
-        opener = urllib.request.build_opener(proxy_handler)
-        # header配置
-        opener.addheaders = [
-            ("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
-        ]
-        urllib.request.install_opener(opener)
-        # 下载文件
-        print(uri, save_path)
-        urllib.request.urlretrieve(uri, save_path, _progress)
-        # urllib.request.urlretrieve(uri, save_path)
-    except Exception as e:
-        logger.exception(e)
-        # print(repr(e))
+    # urllib配置代理
+    proxy_handler = urllib.request.ProxyHandler({
+        "http": "http://127.0.0.1:7890/",
+        "https": "https://127.0.0.1:7890/"
+    })
+    opener = urllib.request.build_opener(proxy_handler)
+    # header配置
+    opener.addheaders = [
+        ("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+    ]
+    urllib.request.install_opener(opener)
+    # 下载文件
+    print(uri, save_path)
+    urllib.request.urlretrieve(uri, save_path, _progress)
     time.sleep(1)
