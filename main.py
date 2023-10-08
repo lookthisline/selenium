8aa2b31e95285c62324f38b094b468f2c1942fdf
lookthisline <liujiawen.personal@outlook.com>
2023-10-08 13:49:45 +0800

    fixed: 修复备选文件名未使用.分割后缀

--- main.py
+++ main.py
@@ -25,9 +25,16 @@ def save_pic(res: list = []):
         # 获取文件后缀
         suffix = item['pic_uri'].split(".")[-1]
         file_name = item['pic_title'][:250] + "." + suffix  # 限制文件名长度，预留5个字符给后缀
+        file_name = re.sub(r'[\\/:*?"<>|]', '', file_name)  # 去除非法字符
         # 备选名称
         file_name_2 = re.findall(
-            '\d+', item["page_link"])[0]  # 提取其中的数字部分
+            r'\d+', item["page_link"])[0]  # 提取其中的数字部分
+
+        # 提前校验备选名称是否已存在
+        if os.path.exists(SAVE_DIR + file_name_2 + "." + suffix):
+            print("文件已存在")
+            return
+
         if not file_name_2:
             # 使用hash时间字符串为文件名
             file_name_2 = hashlib.md5(
@@ -37,7 +44,7 @@ def save_pic(res: list = []):
         except OSError as e:
             logger.error(e)
             save_media(item["pic_uri"], SAVE_DIR +
-                       file_name_2 + suffix)  # 保存到备选名称
+                       file_name_2 + "." + suffix)  # 保存到备选名称
         except Exception as e:
             logger.exception(e)
 
@@ -51,6 +58,9 @@ if __name__ == '__main__':
         driver.get(url)  # home page
         # ----------
 
+        # print driver uri
+        logger.debug(driver.current_url)
+
         res = [i for i in get_element_by_xpath(driver) if i]
         session: Session = next(sqlite_session())
         save_page_data(session, res)  # save main page data
@@ -62,11 +72,13 @@ if __name__ == '__main__':
 
         while current_page < max_read_page:
             # next page
-            d = next_page(current_driver)
-            res = [i for i in get_element_by_xpath(d) if i]
+            next_driver = next_page(current_driver)
+            # print driver uri
+            logger.debug(next_driver.current_url)
+            res = [i for i in get_element_by_xpath(next_driver) if i]
             save_page_data(session, res)
             save_pic(res)
-            current_driver = d
+            current_driver = next_driver
             current_page += 1
 
         # ----------
